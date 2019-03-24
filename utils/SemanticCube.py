f061475ffdfc84b9d7634b8c7fc6a030140e2818
jgalvan <galvan.vil@gmail.com>
2019-03-23 20:03:39 -0600

    Adds semantic cube

---
+++ utils/SemanticCube.py
@@ -0,0 +1,278 @@
+from enum import Enum
+
+class DataType(Enum):
+    """Enum to represent options for data types"""
+
+    INT = "int"
+    FLOAT = "float"
+    BOOL = "bool"
+    STRING = "string"
+    ERROR = "error"
+
+class SemanticCube:
+    """Represents arithmetic and boolean operation return types."""
+
+    @staticmethod
+    def check(oper: str, left_op: DataType, right_op: DataType) -> DataType:
+        """Checks return type for operation, returns DataType.ERROR if operation is invalid."""
+
+        cube = {
+            # -------------------------------------------------
+            # INT
+            # -------------------------------------------------
+
+            ('+', DataType.INT, DataType.INT): DataType.INT,
+            ('+', DataType.INT, DataType.FLOAT): DataType.FLOAT,
+            ('+', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('+', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('-', DataType.INT, DataType.INT): DataType.INT,
+            ('-', DataType.INT, DataType.FLOAT): DataType.FLOAT,
+            ('-', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('-', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('/', DataType.INT, DataType.INT): DataType.FLOAT,
+            ('/', DataType.INT, DataType.FLOAT): DataType.FLOAT,
+            ('/', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('/', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('*', DataType.INT, DataType.INT): DataType.INT,
+            ('*', DataType.INT, DataType.FLOAT): DataType.FLOAT,
+            ('*', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('*', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('>', DataType.INT, DataType.INT): DataType.BOOL,
+            ('>', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('>', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('>', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('<', DataType.INT, DataType.INT): DataType.BOOL,
+            ('<', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('<', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('<', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('==', DataType.INT, DataType.INT): DataType.BOOL,
+            ('==', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('==', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('==', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('<=', DataType.INT, DataType.INT): DataType.BOOL,
+            ('<=', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('<=', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('<=', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('>=', DataType.INT, DataType.INT): DataType.BOOL,
+            ('>=', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('>=', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('>=', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('!=', DataType.INT, DataType.INT): DataType.BOOL,
+            ('!=', DataType.INT, DataType.FLOAT): DataType.BOOL,
+            ('!=', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('!=', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('&&', DataType.INT, DataType.INT): DataType.ERROR,
+            ('&&', DataType.INT, DataType.FLOAT): DataType.ERROR,
+            ('&&', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('&&', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            ('||', DataType.INT, DataType.INT): DataType.ERROR,
+            ('||', DataType.INT, DataType.FLOAT): DataType.ERROR,
+            ('||', DataType.INT, DataType.BOOL): DataType.ERROR,
+            ('||', DataType.INT, DataType.STRING): DataType.ERROR,
+
+            # -------------------------------------------------
+            # FLOAT
+            # -------------------------------------------------
+
+            ('+', DataType.FLOAT, DataType.INT): DataType.FLOAT,
+            ('+', DataType.FLOAT, DataType.FLOAT): DataType.FLOAT,
+            ('+', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('+', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('-', DataType.FLOAT, DataType.INT): DataType.FLOAT,
+            ('-', DataType.FLOAT, DataType.FLOAT): DataType.FLOAT,
+            ('-', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('-', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('/', DataType.FLOAT, DataType.INT): DataType.FLOAT,
+            ('/', DataType.FLOAT, DataType.FLOAT): DataType.FLOAT,
+            ('/', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('/', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('*', DataType.FLOAT, DataType.INT): DataType.FLOAT,
+            ('*', DataType.FLOAT, DataType.FLOAT): DataType.FLOAT,
+            ('*', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('*', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('>', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('>', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('>', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('>', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('<', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('<', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('<', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('<', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('==', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('==', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('==', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('==', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('<=', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('<=', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('<=', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('<=', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('>=', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('>=', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('>=', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('>=', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('!=', DataType.FLOAT, DataType.INT): DataType.BOOL,
+            ('!=', DataType.FLOAT, DataType.FLOAT): DataType.BOOL,
+            ('!=', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('!=', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('&&', DataType.FLOAT, DataType.INT): DataType.ERROR,
+            ('&&', DataType.FLOAT, DataType.FLOAT): DataType.ERROR,
+            ('&&', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('&&', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            ('||', DataType.FLOAT, DataType.INT): DataType.ERROR,
+            ('||', DataType.FLOAT, DataType.FLOAT): DataType.ERROR,
+            ('||', DataType.FLOAT, DataType.BOOL): DataType.ERROR,
+            ('||', DataType.FLOAT, DataType.STRING): DataType.ERROR,
+
+            # -------------------------------------------------
+            # BOOL
+            # -------------------------------------------------
+
+            ('+', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('+', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('+', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('+', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('-', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('-', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('-', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('-', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('/', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('/', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('/', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('/', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('*', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('*', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('*', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('*', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('>', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('>', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('>', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('>', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('<', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('<', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('<', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('<', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('==', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('==', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('==', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('==', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('<=', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('<=', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('<=', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('<=', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('>=', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('>=', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('>=', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('>=', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('!=', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('!=', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('!=', DataType.BOOL, DataType.BOOL): DataType.ERROR,
+            ('!=', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('&&', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('&&', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('&&', DataType.BOOL, DataType.BOOL): DataType.BOOL,
+            ('&&', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            ('||', DataType.BOOL, DataType.INT): DataType.ERROR,
+            ('||', DataType.BOOL, DataType.FLOAT): DataType.ERROR,
+            ('||', DataType.BOOL, DataType.BOOL): DataType.BOOL,
+            ('||', DataType.BOOL, DataType.STRING): DataType.ERROR,
+
+            # -------------------------------------------------
+            # STRING
+            # -------------------------------------------------
+
+            ('+', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('+', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('+', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('+', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('-', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('-', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('-', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('-', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('/', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('/', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('/', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('/', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('*', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('*', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('*', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('*', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('>', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('>', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('>', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('>', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('<', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('<', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('<', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('<', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('==', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('==', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('==', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('==', DataType.STRING, DataType.STRING): DataType.BOOL,
+
+            ('<=', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('<=', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('<=', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('<=', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('>=', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('>=', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('>=', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('>=', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('!=', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('!=', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('!=', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('!=', DataType.STRING, DataType.STRING): DataType.BOOL,
+
+            ('&&', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('&&', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('&&', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('&&', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+            ('||', DataType.STRING, DataType.INT): DataType.ERROR,
+            ('||', DataType.STRING, DataType.FLOAT): DataType.ERROR,
+            ('||', DataType.STRING, DataType.BOOL): DataType.ERROR,
+            ('||', DataType.STRING, DataType.STRING): DataType.ERROR,
+
+        }
+
+        return cube[(oper, left_op, right_op)]
