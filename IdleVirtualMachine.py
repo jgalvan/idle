763ecd6c35796b81964e373c4e6cc2c94f552608
jgalvan <galvan.vil@gmail.com>
2019-04-10 11:54:45 -0500

    Add jumps to object functions

--- IdleVirtualMachine.py
+++ IdleVirtualMachine.py
@@ -1,6 +1,5 @@
 from utils.OperationCode import OperationCode
-from virtual_machine_utils.ClassMemory import ClassMemory
-from virtual_machine_utils.Memory import Memory
+from virtual_machine_utils.Memory import Memory, ClassMemory
 from utils.Stack import Stack
 
 class IdleVirtualMachine():
@@ -8,11 +7,16 @@ class IdleVirtualMachine():
         self.__const_quadruples = const_quadruples
         self.__quadruples = quadruples
         self.__memory_stack = Stack()
+        self.__next_class_stack = Stack()
 
         curr_class = ClassMemory()
         curr_class.era_func(self.__quadruples[0][3])
         curr_class.goto_next_func()
         self.__memory_stack.push(curr_class)
+
+    @property
+    def current_memory(self):
+        return self.__memory_stack.peek()
 
     def run(self):
         self.init_consts()
@@ -21,27 +25,37 @@ class IdleVirtualMachine():
         next_quad = self.next_instruction()
         while next_quad != None:
             if OperationCode(next_quad[0]) == OperationCode.GOTO:
-                self.__memory_stack.peek().goto(next_quad[3])
+                self.current_memory.goto(next_quad[3])
             elif OperationCode(next_quad[0]) == OperationCode.ASSIGN:
-                value = self.__memory_stack.peek().get_value(next_quad[1])
-                self.__memory_stack.peek().set_value(value, next_quad[3])
+                value = self.current_memory.get_value(next_quad[1])
+                self.current_memory.set_value(value, next_quad[3])
             elif OperationCode(next_quad[0]) == OperationCode.ERA:
-                self.__memory_stack.peek().era_func(next_quad[1])
+                if next_quad[2] != None:
+                    obj = self.current_memory.get_value(next_quad[2])
+                    obj.era_func(next_quad[1])
+                    self.__next_class_stack.push(obj)
+                else:
+                    self.current_memory.era_func(next_quad[1])
             elif OperationCode(next_quad[0]) == OperationCode.GOSUB:
-                self.__memory_stack.peek().goto_next_func()
+                if not self.__next_class_stack.isEmpty():
+                    self.__memory_stack.push(self.__next_class_stack.pop())
+                self.current_memory.goto_next_func()
             elif OperationCode(next_quad[0]) == OperationCode.ENDPROC:
-                self.__memory_stack.peek().end_func()
+                self.current_memory.end_func()
 
             print("===== EXECUTED: " + str(next_quad) + " =========")
-            print(self.__memory_stack.peek())
+            print(self.current_memory)
             
             next_quad = self.next_instruction()
 
     def next_instruction(self):
-        counter = self.__memory_stack.peek().next_instruction()
+        counter = self.current_memory.next_instruction()
         if counter != None:
             return self.__quadruples[counter]
-        
+        elif self.__memory_stack.size() > 1:
+            self.__memory_stack.pop()
+            return self.next_instruction()
+
         return None
 
     def init_consts(self):
