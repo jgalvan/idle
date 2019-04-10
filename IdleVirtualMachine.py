72400db9067956d22824c5091f3cbde411483848
jgalvan <galvan.vil@gmail.com>
2019-04-10 09:58:24 -0500

    Add local memory

--- IdleVirtualMachine.py
+++ IdleVirtualMachine.py
@@ -8,16 +8,27 @@ class IdleVirtualMachine():
         self.__const_quadruples = const_quadruples
         self.__quadruples = quadruples
         self.__memory_stack = Stack()
-        self.__memory_stack.push(ClassMemory())
+        
+        curr_mem = ClassMemory()
+        curr_mem.era_func()
+        self.__memory_stack.push(curr_mem)
 
     def run(self):
         self.init_consts()
         # RUN
+
+        for quad in self.__quadruples:
+            if OperationCode(quad[0]) == OperationCode.ASSIGN:
+                value = self.__memory_stack.peek().get_value(quad[1])
+                self.__memory_stack.peek().set_value(value, quad[3])
+            elif OperationCode(quad[0]) == OperationCode.ERA:
+                self.__memory_stack.peek().era_func()
+        
+
+        print(self.__memory_stack.peek())
 
     def init_consts(self):
         temp = Memory()
 
         for quad in self.__const_quadruples:
             temp.set_value(quad[1], quad[3])
-
-        print(Memory.CONSTANTS)
