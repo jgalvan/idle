c8e3a6d9ffc494153419294a1b5965bf548a0181
jgalvan <galvan.vil@gmail.com>
2019-04-10 12:02:55 -0500

    Refactor to instruction set dict

--- IdleVirtualMachine.py
+++ IdleVirtualMachine.py
@@ -19,34 +19,31 @@ class IdleVirtualMachine():
         return self.__memory_stack.peek()
 
     def run(self):
+        instruction_set = {
+            OperationCode.GOTO: self.run_goto,
+            OperationCode.ASSIGN: self.run_assign,
+            OperationCode.ERA: self.run_era,
+            OperationCode.GOSUB: self.run_gosub,
+            OperationCode.ENDPROC: self.run_endproc
+        }
+
         self.init_consts()
-        # RUN
 
         next_quad = self.next_instruction()
         while next_quad != None:
-            if OperationCode(next_quad[0]) == OperationCode.GOTO:
-                self.current_memory.goto(next_quad[3])
-            elif OperationCode(next_quad[0]) == OperationCode.ASSIGN:
-                value = self.current_memory.get_value(next_quad[1])
-                self.current_memory.set_value(value, next_quad[3])
-            elif OperationCode(next_quad[0]) == OperationCode.ERA:
-                if next_quad[2] != None:
-                    obj = self.current_memory.get_value(next_quad[2])
-                    obj.era_func(next_quad[1])
-                    self.__next_class_stack.push(obj)
-                else:
-                    self.current_memory.era_func(next_quad[1])
-            elif OperationCode(next_quad[0]) == OperationCode.GOSUB:
-                if not self.__next_class_stack.isEmpty():
-                    self.__memory_stack.push(self.__next_class_stack.pop())
-                self.current_memory.goto_next_func()
-            elif OperationCode(next_quad[0]) == OperationCode.ENDPROC:
-                self.current_memory.end_func()
+            instruction = instruction_set[OperationCode(next_quad[0])]
+            instruction(next_quad)
 
             print("===== EXECUTED: " + str(next_quad) + " =========")
             print(self.current_memory)
             
             next_quad = self.next_instruction()
+
+    def init_consts(self):
+        temp = Memory()
+
+        for quad in self.__const_quadruples:
+            temp.set_value(quad[1], quad[3])
 
     def next_instruction(self):
         counter = self.current_memory.next_instruction()
@@ -57,9 +54,28 @@ class IdleVirtualMachine():
             return self.next_instruction()
 
         return None
+    
+    # INSTRUCTION SET FUNCTIONS
 
-    def init_consts(self):
-        temp = Memory()
+    def run_goto(self, quad):
+        self.current_memory.goto(quad[3])
 
-        for quad in self.__const_quadruples:
-            temp.set_value(quad[1], quad[3])
+    def run_assign(self, quad):
+        value = self.current_memory.get_value(quad[1])
+        self.current_memory.set_value(value, quad[3])
+
+    def run_era(self, quad):
+        if quad[2] != None:
+            obj = self.current_memory.get_value(quad[2])
+            obj.era_func(quad[1])
+            self.__next_class_stack.push(obj)
+        else:
+            self.current_memory.era_func(quad[1])
+    
+    def run_gosub(self, quad):
+        if not self.__next_class_stack.isEmpty():
+            self.__memory_stack.push(self.__next_class_stack.pop())
+        self.current_memory.goto_next_func()
+    
+    def run_endproc(self, quad):
+        self.current_memory.end_func()
